afb018e6c45490362e762afa8271ee7fe0870e7f
anggaihza <angga.asep98@gmail.com>
2023-11-28 07:16:05 +0700

    Detail Product

--- server/routes/product.js
+++ server/routes/product.js
@@ -5,7 +5,6 @@ const Product = require("../models/product")
 
 productRouter.get('/api/products', auth, async (req, res) => {
     try {
-        console.log(req.query.category);
         const products = await Product.find({ category: req.query.category })
         res.json(products)
     } catch (error) {
